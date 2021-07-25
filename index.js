97667ea7e8c6a2342d8b9a77172cb3cdc8abae1c
Houssnou Kabore <hkabore@wsits.com>
2021-07-25 12:40:03 -0400

    Added Product Details Endpoint

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 const express = require('express');
 const request = require('request-promise');
+const listEndpoints = require('express-list-endpoints');
+
+require("dotenv").config();
 
 const app = express();
 const PORT = process.env.PORT || 5800;
+
+const apiKey = process.env.apiKey;
+
+const baseUrl = `http://api.scraperapi.com?api_key=${apiKey}&autoparse=true`;
+
 
 app.use(express.json());
 
@@ -10,6 +18,21 @@ app.get('/', (req, res) => {
     res.send('Welcome to Amazon Scraper API.')
 });
 
+// GET Product Details
+app.get('/products/:productId', async (req, res) => {
+    const { productId } = req.params;
+
+    try {
+
+        const response = await request(`${baseUrl}&url=https://www.amazon.com/dp/${productId}`);
+        res.json(response);
+
+    } catch (error) {
+        res.json(error);
+    }
+});
+
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+    console.log("App running on server http://localhost:" + PORT);
+    console.log(listEndpoints(app));
 })
